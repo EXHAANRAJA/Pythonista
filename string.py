cd785a56deabc40c6e846c1a15a782147176dee0
AHSAN UL HAQ <rajaahsanulhaq3@gmail.com>
2022-10-10 13:52:50 +0500

    String & Arithmatic Operation

---
+++ string.py
@@ -0,0 +1,28 @@
+
+import math as m
+
+msg = "Hello Pakistan"
+print(len(msg)) # To check length of Python String
+print(msg.capitalize()) # To convert string into Capitalize,lower,upper
+print(msg.upper())
+print(msg.lower())
+print(msg.replace("Pakistan","my country Pakistan"))
+
+# Evaluate Boolean Expression
+print('Pakistan' in msg)
+
+# Arithmatic Expression
+
+print(10+3)
+print(10-3)
+print(10*3)
+print(10**3)
+print(10/3)
+print(10//3)
+print(10%3)
+
+# Print factorial throuh math package
+print(m.factorial(5))
+
+
+
