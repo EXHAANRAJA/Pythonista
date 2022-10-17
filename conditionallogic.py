1bee6fd0503258302030b2076f5c7e6e28cfde98
AHSAN UL HAQ <rajaahsanulhaq3@gmail.com>
2022-10-17 11:52:59 +0500

    Condition/Logical Formulas

---
+++ conditionallogic.py
@@ -0,0 +1,19 @@
+# Logical Operator
+name = 'AHSAN'
+secondname = "AKRAM"
+
+if name == 'AHSAN' and secondname == 'AKRAM':
+    print("Correct Guess")
+else:
+    print("wrong guess attempt again")
+
+# Comparison Operator
+
+ishot = [True,False,True,True,False,False,False]
+if ishot[1]:
+    print("Very hot days in a month")
+else:
+    print("Good weather today")
+
+
+
